7ee852ce439a318717d3b8fb97642b8fa8284b14
rum <rumsnownsk@gmail.com>
2025-07-23 16:44:52 +0700

    fix config.php

--- config/config.php
+++ config/config.php
@@ -9,9 +9,9 @@ return [
     'db' => [
         'host' => 'localhost',
         'port' => '3306',
-        'username' => 'admin',
-        'password' => 'C9qCM8ZxQDIk41GF',
-        'dbname' => 'db_sandbox',
+        'username' => '',
+        'password' => '',
+        'dbname' => '',
         'charset' => 'utf8',
         'options' => [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
